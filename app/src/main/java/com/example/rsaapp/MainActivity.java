60daf744cc64a80d6d79a95a816334a2fa91db65
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-07-23 13:41:10 +0530

    minor changes

--- app/src/main/java/com/example/rsaapp/MainActivity.java
+++ app/src/main/java/com/example/rsaapp/MainActivity.java
@@ -22,7 +22,7 @@ import javax.crypto.NoSuchPaddingException;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txt_pNumber, txt_message, txt_rsa_output;
+    EditText txt_pNumber, txt_message, txt_output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +32,20 @@ public class MainActivity extends AppCompatActivity {
 
         txt_message= findViewById(R.id.txt_message);
         txt_pNumber= findViewById(R.id.txt_phone_number);
-        txt_rsa_output = findViewById(R.id.txt_rsa_output);
+        txt_output = findViewById(R.id.txt_output);
     }
 
 
-
-    public void btn_send(View view) {
+    public void send(View view) {
+        final int permissionCheck= ContextCompat.checkSelfPermission(this,Manifest.permission.SEND_SMS);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+            MyMessage();
+        }
+        else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},0);
+        }
+    }
+    /*public void btn_send(View view) {
         final int permissionCheck= ContextCompat.checkSelfPermission(this,Manifest.permission.SEND_SMS);
         if(permissionCheck == PackageManager.PERMISSION_GRANTED){
             MyMessage();
@@ -72,16 +80,6 @@ public class MainActivity extends AppCompatActivity {
 
         txt_rsa_output.setText(Enc_text);
 
-        /*if (!txt_pNumber.getText().toString().equals("") || !txt_message.getText().toString().equals("")) {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNumber, null, Enc_text, null, null);
-            Toast.makeText(this, "Message sent", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(this, "Please enter a message", Toast.LENGTH_SHORT).show();
-
-        }*/
-
         AESEncyption dec = new AESEncyption();
         try {
             Dec_text = dec.decrypt(Enc_text);
@@ -110,11 +108,12 @@ public class MainActivity extends AppCompatActivity {
         }
         Toast.makeText(this,Dec_text,Toast.LENGTH_SHORT).show();
     }
-
+*/
     private void MyMessage() {
         String phoneNumber = txt_pNumber.getText().toString().trim();
         String Message = txt_message.getText().toString().trim();
         String Enc_text = "";
+        String Dec_text="";
 
         AESEncyption enc = new AESEncyption();
         try {
@@ -133,17 +132,35 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        txt_rsa_output.setText(Enc_text);
+        txt_output.setText(Enc_text);
+
+        AESEncyption dec = new AESEncyption();
+        try {
+            Dec_text = dec.decrypt(Enc_text);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         if (!txt_pNumber.getText().toString().equals("") || !txt_message.getText().toString().equals("")) {
             SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNumber, null, Enc_text, null, null);
+            smsManager.sendTextMessage(phoneNumber, null, Dec_text, null, null);
             Toast.makeText(this, "Message sent", Toast.LENGTH_SHORT).show();
         }
         else{
             Toast.makeText(this, "Please enter a message", Toast.LENGTH_SHORT).show();
 
         }
+        Toast.makeText(this,Dec_text,Toast.LENGTH_SHORT).show();
     }
 
     @Override
